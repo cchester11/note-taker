bff2b337d15ac904c247f8e8cfe47cfce7393d64
Charles <cchester1@angelo.edu>
2021-05-04 15:22:07 -0500

    ..

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const db = require("../../db/db.json");
+const fs = require('fs');
+const path = require('path');
+const createNewNote = require("../../lib/notes");
+
+//get request done.
+//need to create a lib folder for the functions used in this get route
+router.get('/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, data) => {
+    if(err) throw err
+    res.json(JSON.parse(data))
+  })
+})
+
+//post request done.
+//post the new note to JSON by using the req.body in a createNewNote function and post the new note as a response
+router.post('/notes', function (req, res) {
+  // console.log(notesDb)
+  if (db.length == 0) {
+    req.body.id = "0";
+  } else {
+    req.body.id = db.length.toString()
+  }
+  db.push(req.body)
+  res.json(req.body)
+  createNewNote(db)
+
+})
+
+module.exports = router;
