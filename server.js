0cca39465b70e2c646ba5a77000c76dbaf51ff13
Charles <cchester1@angelo.edu>
2021-05-04 22:02:24 -0500

    ..

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-
+const path = require('path');
 const PORT = process.env.PORT || 3002;
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
